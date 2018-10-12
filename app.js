919ee2787ae203a658a7804958ee2714eafd7d12
Stephen Ouyang <xisouyang@gmail.com>
2018-10-12 11:13:41 -0700

    user can post

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 const express = require('express')
 const app = express()
-var exphbs = require('express-handlebars')
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/Storyteller_Blog', {useNewUrlParser: true})
+var exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const Post = mongoose.model('Post', {
+  title: String,
+  description: String
+});
+
+
+// INDEX
 app.get('/', (req, res) => {
-    // res.send('Hello-World')
-    res.render('hello', {})
+  Post.find()
+    .then(posts => {
+      res.render('post-index', { posts: posts });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+// NEW
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new', {});
+})
+
+// CREATE
+app.post('/posts', (req, res) => {
+  console.log(req.body);
+  Post.create(req.body).then((post) => {
+      res.redirect('/')
+  }).catch((err) => {
+      console.log(err.message)
+  })
+  // res.render('reviews-new', {});
 })
 
 app.listen(3000, () => {
